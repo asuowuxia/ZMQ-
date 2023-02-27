289f48154900f06ad4151a24ca6541f4eb36e771
asuowuxia <1369833786@qq.com>
2023-02-27 11:45:19 +0800

    IPC之旅，开始zeromq的开发，消息队列，消息中间件，我来裸泳了

---
+++ zmq_projects/zmq_projects/zmq_projects.cpp
@@ -0,0 +1,18 @@
+﻿// zmq_projects.cpp: 定义应用程序的入口点。
+//
+
+#include "zmq_projects.h"
+#include "include/zmq.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	int major, minor, patch;
+	zmq_version(&major, &minor, &patch);
+
+	cout << major << " " << minor << " " << patch << endl;
+
+	return 0;
+}
